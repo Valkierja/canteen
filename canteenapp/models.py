7f10a617395fd097e369a91a4d41df523f9030ee
KSroido <ksxmyqj@gmail.com>
2022-04-20 15:57:54 +0800

    slightly change

--- canteenapp/models.py
+++ canteenapp/models.py
@@ -9,11 +9,12 @@ class DinnerTable(models.Model):
     a2 = models.CharField(verbose_name="炒白菜（3.4元/份）",max_length=10,default=0)
     a3 = models.CharField(verbose_name="红烧猪蹄（18元/只）",max_length=10,default=0)
     a4 = models.CharField(verbose_name="可乐（3.3元/瓶）",max_length=10,default=0)
-
     price = 0
     def getPrice(self):
         self.price = 0
         self.price = int(self.a1[:]) * 1.1 + int(self.a2[:]) * 3.4 + int(self.a3[:]) *  18 + int(self.a4[:]) * 3.3
+        print(self.price+"\n")
+        print(self.date)
         return
     
     def __str__(self) -> str:
