2d41f6f683a1dc8502a9e8f7d7af17c5b60798cd
KSroido <ksxmyqj@gmail.com>
2022-04-20 14:24:36 +0800

    done release

--- canteenapp/admin.py
+++ canteenapp/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from canteenapp.models import ChaoCai, DinnerTable
-admin.site.register(ChaoCai)
+from canteenapp.models import *
+# admin.site.register(ChaoCai)
 admin.site.register(DinnerTable)
--- canteenapp/models.py
+++ canteenapp/models.py
@@ -1,18 +1,33 @@
+from cProfile import label
 from django.db import models
 
 class DinnerTable(models.Model):
     ID = models.CharField(primary_key=True,max_length=64)
     date = models.DateTimeField(auto_now_add=True)
+
+    a1 = models.CharField(verbose_name="米饭（1.1元/碗）",max_length=10,default=0)
+    a2 = models.CharField(verbose_name="炒白菜（3.4元/份）",max_length=10,default=0)
+    a3 = models.CharField(verbose_name="红烧猪蹄（18元/只）",max_length=10,default=0)
+    a4 = models.CharField(verbose_name="可乐（3.3元/瓶）",max_length=10,default=0)
+
+    price = 0
+    def getPrice(self):
+        self.price = 0
+        self.price = int(self.a1[:]) * 1.1 + int(self.a2[:]) * 3.4 + int(self.a3[:]) *  18 + int(self.a4[:]) * 3.3
+        return
+    
     def __str__(self) -> str:
         return self.ID
 
-class ChaoCai(models.Model): #炒菜
-    table = models.ManyToManyField(DinnerTable)
-    name = models.CharField(max_length=64)
-    num = models.PositiveIntegerField()
-    price = models.FloatField(primary_key=True)
 
-    def __str__(self) -> str:
-        return self.name
+
+# class ChaoCai(models.Model): #炒菜
+#     table = models.ManyToManyField(DinnerTable)
+#     name = models.CharField(max_length=64)
+#     num = models.PositiveIntegerField()
+#     price = models.FloatField(primary_key=True)
+
+#     def __str__(self) -> str:
+#         return self.name
     
 
